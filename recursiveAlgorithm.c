1015cf1b8d4740c8bca49f7f005f274e852c45eb
Nicolas Orefice <123935503+Testeh69@users.noreply.github.com>
2023-09-29 00:13:17 +0200

    Add files via upload

--- recursiveAlgorithm.c
+++ recursiveAlgorithm.c
@@ -1,14 +1,94 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
-
+//function calcul
 float puissance(float x, int n);
+int factorielle(int x);
+int detectPalyndrome(char phr[], int debut, int fin);
+
+
+//function call
 void callPuissance();
+void callFactorielle();
+void callDetectPalyndrome();
 
 
 int main(){
-    callPuissance();
+    callDetectPalyndrome();
+    return 0;
 }
+
+void callDetectPalyndrome(){
+    char phrase[100];
+    int i=0, isPalindrome;
+    printf("Entrer une phrase : ");
+    while((phrase[i++] = getchar()) != '\n');
+    phrase[--i] = '\0';
+    isPalindrome = detectPalyndrome(phrase,0,i-1);
+    if (isPalindrome){
+        printf("C'est un palindrome");
+    }
+    else{
+        printf("Ce n'est pas un palindrome\n");
+    }
+}
+
+
+
+
+int detectPalyndrome(char phr[], int debut, int fin){
+    static int compteur = 0;
+    while (phr[debut] == ' '){
+        debut++;
+    }
+    while( phr[fin] == ' '){
+        fin--;
+    }
+    printf("appel=%d",++compteur);
+    printf("début=%d '%c' ",debut, phr[debut]);
+    printf("fin=%d '%c'",fin, phr[fin]);
+    if (debut>fin){
+        return 1;
+    }
+    else if (phr[debut] != phr[fin]){
+        return 0;
+    }
+    else{
+        return detectPalyndrome(phr,debut+1, fin -1);
+    }
+
+}
+
+
+
+
+void callFactorielle(){
+    int m;
+    printf("entrer le nombre qui subit le produit factorielle => ");
+    scanf("%d", &m);//call 
+    int resultat = factorielle(m);
+    printf("%d", resultat);
+}
+
+int factorielle(int x){
+    int resultat;
+    if( x == 1){
+        resultat = 1;
+    }
+    else{
+        resultat = factorielle(x-1)*x;
+    }
+    return resultat;
+
+}
+
+
+
+
+
+
 
 
 
